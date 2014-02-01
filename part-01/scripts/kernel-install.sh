3f124ed2fefc8a915766b4f11f562224a645a320
dezi <dezi@kappa-mm.de>
2014-02-01 14:35:35 +0100

    Check in.

--- part-01/scripts/kernel-install.sh
+++ part-01/scripts/kernel-install.sh
@@ -6,7 +6,7 @@ sudo cp -f /boot/uInitrd /boot/uInitrd-`uname -r`
 sudo cp arch/arm/boot/zImage /boot/zImage-`cat include/config/kernel.release`
 sudo cp .config /boot/config-`cat include/config/kernel.release`
 
-sudo update-initramfs -c -k `cat include/config/kernel.release`
+sudo update-initramfs -c -t -k `cat include/config/kernel.release`
 
 sudo mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n uInitrd -d /boot/initrd.img-`cat include/config/kernel.release` /boot/uInitrd-`cat include/config/kernel.release`
 
