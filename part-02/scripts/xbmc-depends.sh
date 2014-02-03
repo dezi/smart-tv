7c5f29631c25080492d067797b2bbce1127787c1
dezi <dezi@kappa-mm.de>
2014-02-03 08:57:19 +0100

    Check in.

---
+++ part-02/scripts/xbmc-depends.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Install XBMC build dependencies.
+# By dezi
+
+sudo apt-get install \
+	libvdpau-dev ccache autopoint libltdl-dev git-core build-essential \
+	gawk pmount libtool nasm yasm automake cmake gperf zip unzip bison \
+	libsdl-dev libsdl-image1.2-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev \
+	libfribidi-dev liblzo2-dev libfreetype6-dev libsqlite3-dev libogg-dev \
+	libasound2-dev python-sqlite libglew-dev libcurl3 libcurl4-gnutls-dev \
+	libxrandr-dev libxrender-dev libmad0-dev libogg-dev libvorbisenc2 \
+	libsmbclient-dev libmysqlclient-dev libpcre3-dev libdbus-1-dev \
+	libhal-dev libhal-storage-dev libjasper-dev libfontconfig-dev \
+	libbz2-dev libboost-dev libenca-dev libxt-dev libxmu-dev libpng-dev \
+	libjpeg-dev mesa-utils libcdio-dev libsamplerate-dev libflac-dev \
+	libiso9660-dev libass-dev libssl-dev fp-compiler gdc libmpeg2-4-dev \
+	libmicrohttpd-dev libmodplug-dev libssh-dev gettext cvs python-dev \
+	libyajl-dev libboost-thread-dev libplist-dev libusb-dev libudev-dev \
+	libtinyxml-dev libcap-dev curl swig default-jre libavcodec-dev \
+	libavfilter-dev libavformat-dev libavutil-dev libbluetooth-dev \
+	libbluray-dev libbluray1 libcec-dev libcurl4-gnutls-dev libcwiid-dev \
+	libcwiid1 libnfs-dev libpostproc-dev libshairport-dev libswscale-dev \
+	libva-dev libva-egl1 libva-tpi1 libmp3lame-dev libtiff-dev pkg-config
+
