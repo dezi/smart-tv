3b7732b3c2a77011e325c6770c69b2c226da4c57
dezi <dezi@kappa-mm.de>
2014-02-03 10:13:22 +0100

    Check in.

--- part-02/scripts/xbmc-configure.sh
+++ part-02/scripts/xbmc-configure.sh
@@ -4,7 +4,14 @@
 
 CPPFLAGS="-I/usr/include/samba-4.0"
 
-CFLAGS="-march=native -mfloat-abi=hard -mfpu=neon -ftree-vectorize -mvectorize-with-neon-quad -ffast-math -mcpu=cortex-a9 -mtune=cortex-a9 -mthumb -O2 -pipe -fstack-protector --param=ssp-buffer-size=4 -D_FORTIFY_SOURCE=2"
+CFLAGS="-march=native -mfloat-abi=hard \
+        -mfpu=neon -ftree-vectorize \
+        -mvectorize-with-neon-quad \
+        -ffast-math -mcpu=cortex-a9 \
+        -mtune=cortex-a9 -mthumb \
+        -O2 -pipe -fstack-protector \
+        --param=ssp-buffer-size=4 \
+        -D_FORTIFY_SOURCE=2"
 
 export CPPFLAGS
 export CFLAGS
