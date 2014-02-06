3652cbcffc9dc4f72238bf82a5555914d7fcb6a9
dezi <dezi@kappa-mm.de>
2014-02-06 20:25:42 +0100

    Check in.

---
+++ part-02/scripts/kernel_wlan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd
+cd install/odroid-3.8.y
+
+rm -f odroid*
+
+wget https://raw.github.com/dezi/smart-tv/master/part-02/configs/$1
+cp $1 arch/arm/configs/$1
+
+wget https://raw.github.com/dezi/smart-tv/master/part-02/patches/rt2x00.dezi.diff
+git apply rt2x00.dezi.diff
+
+make $1
+
+make -j5 zImage modules
+
+sudo make modules_install
+
+sudo cp arch/arm/boot/zImage /boot/zImage-`cat include/config/kernel.release`
+sudo update-initramfs -c -t -k `cat include/config/kernel.release`
+sudo mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n uInitrd -d /boot/initrd.img-`cat include/config/kernel.release` /boot/uInitrd-`cat include/config/kernel.release`
+
+sudo cp /boot/zImage-`cat include/config/kernel.release` /boot/zImage
+sudo cp /boot/uInitrd-`cat include/config/kernel.release` /boot/uInitrd
+
+sudo mv /etc/modprobe.d/blacklist-ralink.conf /etc/modprobe.d/blacklist-ralink.noconf
