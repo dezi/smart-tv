64142cc0bb28536a1133046f16402e119f5777f6
dezi <dezi@kappa-mm.de>
2014-02-20 10:29:41 +0100

    Check in.

---
+++ part-03/scripts/patch_rcb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Rom Collections Browser patch
+# By dezi
+
+wget https://raw.github.com/dezi/smart-tv/master/part-03/patches/mame.xbmc.patch
+
+unzip script.games.rom.collection.browser-2.0.10.zip
+
+cd script.games.rom.collection.browser-2.0.10
+cd resources
+
+git apply ../../mame.xbmc.patch
+
+cd ../..
+
+zip -r script.games.rom.collection.browser-2.0.10-patched.zip script.games.rom.collection.browser
+
+rm -f script.games.rom.collection.browser-2.0.10.zip
+
