71ff171f02ce9b93ec63d41ceae841ba04e252b5
dezi <dezi@kappa-mm.de>
2014-03-05 13:42:04 +0100

    Check in.

---
+++ part-05/scripts/chromium-conf.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Chromiunm Configuration
+# By debian/dezi
+
+# Disable SSE2
+GYP_DEFINES = disable_sse2=1
+
+# Enable all codecs for HTML5 in chromium, depending on which ffmpeg sumo lib
+# is installed, the set of usable codecs (at runtime) will still vary
+GYP_DEFINES += proprietary_codecs=1
+
+# enable compile-time dependency on gnome-keyring
+GYP_DEFINES += use_gnome_keyring=1 linux_link_gnome_keyring=1
+
+# controlling the use of GConf (the classic GNOME configuration
+# and GIO, which contains GSettings (the new GNOME config system)
+GYP_DEFINES += use_gconf=1 use_gio=1
+
+# disable native client (nacl)
+GYP_DEFINES += disable_nacl=1
+
+# do not use embedded third_party/gold as the linker.
+# GYP_DEFINES += linux_use_gold_binary=0 linux_use_gold_flags=0
+
+# disable tcmalloc
+GYP_DEFINES += linux_use_tcmalloc=0
+
+# Use explicit library dependencies instead of dlopen.
+# This makes breakages easier to detect by revdep-rebuild.
+GYP_DEFINES += linux_link_gsettings=1
+
+# Use ARM architecture
+GYP_DEFINES += -DUSE_EABI_HARDFLOAT
+GYP_DEFINES += v8_use_arm_eabi_hardfloat=true
+GYP_DEFINES += arm_fpu=neon
+GYP_DEFINES += arm_float_abi=hard
+GYP_DEFINES += arm_thumb=1
+GYP_DEFINES += armv7=1
+GYP_DEFINES += arm_neon=1
+GYP_DEFINES += target_arch=arm
+
+export GYP_DEFINES
